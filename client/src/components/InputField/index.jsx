af27cac5bb63e6135218a85ef6d573985aee5e1d
Sebastian Stant <sebastianstant@gmail.com>
2019-12-07 18:24:29 -0500

    inputfield and fake price calc

---
+++ client/src/components/InputField/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+    background: #f3eee8;
+    outline: none;
+    font-family: Courier, monospace;
+    color: #009dd1
+    width: 100%;
+    padding: 0px 10px;
+    margin-bottom: 20px;
+`;
+
+const InputField = ({ value, placeholder, onChange }) => {
+    return (<StyledInput value={value} placeholder={placeholder} onChange={onChange} />)
+};
+
+export default InputField;
