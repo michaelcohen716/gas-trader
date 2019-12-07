6a95d97528f5642f4718ef520a4589e4769d2f54
Sebastian Stant <sebastianstant@gmail.com>
2019-12-07 17:31:58 -0500

    CurrentPrice, Account and Button components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
-import getWeb3 from "./getWeb3";
 import cybertruck from './assets/images/cybertruck.png';
 import "./App.css";
 import PercentilesBar from './components/PercentilesBar';
-import Card from './components/Card';
+import CurrentPrice from './components/CurrentPrice';
+import Account from './components/Account';
 
 const App = () => {
-
     return (
         <div className="container">
             <div className="row">
@@ -19,22 +18,12 @@ const App = () => {
                     <PercentilesBar />
                 </div>
                 <div className="col-md-4">
-                    <Card title="Current Price">
-                        <div className="row">
-                            <div className="col-md-12 text-center">
-                                <h1>12.13</h1>
-                            </div>
-                        </div>
-                    </Card>
-                    <Card title="Hi Stranger">
-                        
-                    </Card>
-                    
+                    <CurrentPrice />
+                    <Account />
                 </div>
             </div>
         </div>
     )
-
 };
 
 export default App;
