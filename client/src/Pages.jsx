f313f260d59a9cc8c77cfaeb598862fb6ed7eb2e
Mitchell Opatowsky <opatowsky77@gmail.com>
2019-12-08 13:58:33 -0500

    fixed charts page route

--- client/src/Pages.jsx
+++ client/src/Pages.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./Home";
+import Charts from "./Charts";
 import Orders from "./Orders";
 import About from "./About";
 import { Switch, Route } from "react-router-dom";
@@ -8,6 +9,7 @@ const Pages = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/charts" component={Charts} />
       <Route exact path="/orders" component={Orders} />
       <Route exact path="/about" component={About} />}
     </Switch>
